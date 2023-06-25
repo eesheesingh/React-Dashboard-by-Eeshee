4a8f6ebfd624560ae48142c6c539c7eb6d9fcd93
eesheesingh <chiro.singh9769@gmail.com>
2023-06-26 00:24:42 +0530

    initial commit

---
+++ src/pages/Charts/Line.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Header, LineCharts } from '../../components'
+
+const Line = () => {
+  return (
+    <div className='m-4 md-10 mt-24 p-10 bg-white
+    dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header category="Chart" title="Inflation Rate" />
+      <div className='w-full'>
+        <LineCharts />
+      </div>
+    </div>
+  )
+}
+
+export default Line
